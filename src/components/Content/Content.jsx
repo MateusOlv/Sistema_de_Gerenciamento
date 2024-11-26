fb5e830e308468e20dd42a8446075fde23913f82
Mateus <mateus.olvr2002@gmail.com>
2024-11-25 22:23:52 -0300

    fix: correção da posição dos elementos da página Registrar.jsx

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -15,35 +15,23 @@ const Content = ({ className = "" }) => {
           labelMinWidth="unset"
           placeholder="Insira seu email"
         />
-        <div className={styles.inputField}>
-          <div className={styles.inputFieldBase}>
-            <div className={styles.inputWithLabel}>
-              <div className={styles.label}>Senha</div>
-              <div className={styles.input}>
-                <input
-                  className={styles.content1}
-                  placeholder="Insira sua senha"
-                  type="text"
-                />
-              </div>
-            </div>
-            <div className={styles.hintText}>
-              Deve conter pelo menos 8 caracteres.
-            </div>
-          </div>
-        </div>
-        <div className={styles.inputFieldBase1}>
-          <div className={styles.inputWithLabel1}>
-            <div className={styles.label}>Confirmar Senha</div>
-            <div className={styles.input}>
-              <input
-                className={styles.content1}
-                placeholder="Insira a senha novamente"
-                type="text"
-              />
-            </div>
-          </div>
-        </div>
+        <InputField
+          inputFieldHeight="70px"
+          inputFieldFlex="unset"
+          label="Senha"
+          labelDisplay="unset"
+          labelMinWidth="unset"
+          placeholder="Insira sua senha"
+        />
+
+        <InputField
+          inputFieldHeight="70px"
+          inputFieldFlex="unset"
+          label="Confirmar Senha"
+          labelDisplay="unset"
+          labelMinWidth="unset"
+          placeholder="Insira a senha novamente"
+        />
       </div>
       <button className={styles.button}>
         <div className={styles.buttonBase}>
@@ -53,9 +41,7 @@ const Content = ({ className = "" }) => {
       <div className={styles.row}>
         <div className={styles.text1}>Já tem uma conta?</div>
         <div className={styles.button1}>
-          <div className={styles.buttonBase1}>
             <a href="/login" className={styles.text2}>Login</a>
-          </div>
         </div>
       </div>
     </form>
